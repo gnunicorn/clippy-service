1665656ee2958d81424ceb22191d20e6088d9370
Benjamin Kampmann <ben@create-build-execute.com>
2016-02-18 18:25:32 +0700

    first version with routing

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,19 +6,28 @@ extern crate staticfile;
 
 use std::path::Path;
 
-use iron::{Iron, Request, Response, IronResult};
+use iron::prelude::*;
 use staticfile::Static;
+use router::Router;
+use iron::status;
 
 fn main() {
     let router = router!(
-        get "/github/:repo/:user/:branch.png" => github_handler,
+        get "/github/:user/:repo/:branch/badge.png" => github_handler,
+        get "/github/:user/:repo/badge.png" => github_handler,
         get "/" => Static::new(Path::new("static"))
     );
 
-    Iron::new(router).http("localhost:8080").unwrap();
+    Iron::new(router).http("0.0.0.0:8080").unwrap();
     println!("Check out 8080");
 
-    fn github_handler(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((iron::status::Ok, "Hello World")))
+    fn github_handler(req: &mut Request) -> IronResult<Response> {
+
+        let ref router = req.extensions.get::<Router>().unwrap();
+        let user = router.find("user").unwrap();
+        let repo = router.find("repo").unwrap();
+        let branch = router.find("branch").unwrap_or("master");
+        let resp = format!("There shall be content here for {}/{}:{} ", user, repo, branch);
+        Ok(Response::with((status::Ok, resp)))
     }
 }
